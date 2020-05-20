f69e075b64ebfaa6e3fd42e3afb9e946b4814a55
Vany Serezhkin <ivan@serezhkin.com>
2020-05-20 16:50:56 +0300

    PENV-214: licenses

--- etl/models/models.go
+++ etl/models/models.go
@@ -1,7 +1,7 @@
 // Copyright 2020 Insolar Network Ltd.
 // All rights reserved.
 // This material is licensed under the Insolar License version 1.0,
-// available at https://github.com/insolar/observer/blob/master/LICENSE.md.
+// available at https://github.com/insolar/block-explorer/blob/master/LICENSE.md.
 
 package models
 
