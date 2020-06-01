196e49abd1cc8ccb157b59e3cdf945dec4b33a20
pmoskovkin <pavel.moskovkin@insolar.io>
2020-06-01 12:30:07 +0300

    PENV-288: integration in extractor

--- etl/extractor/mainnet_test.go
+++ etl/extractor/mainnet_test.go
@@ -3,7 +3,7 @@
 // This material is licensed under the Insolar License version 1.0,
 // available at https://github.com/insolar/block-explorer/blob/master/LICENSE.md.
 
-// +build unit
+// +build integration
 
 package extractor
 
