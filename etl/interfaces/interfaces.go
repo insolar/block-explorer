6fb7131efa4452f8e2ba58c1300037490ea5376b
SergeyRadist <14805670+SergeyRadist@users.noreply.github.com>
2020-06-18 19:20:26 +0300

    PENV-257 search jet by jet id (#57)
    * PENV-257 search jet by jet id
    
    * PENV-257 refactoring and fix bug

--- etl/interfaces/interfaces.go
+++ etl/interfaces/interfaces.go
@@ -108,8 +108,9 @@ type StorageFetcher interface {
 	// GetJetDrops returns jetDrops for provided pulse from db.
 	GetJetDrops(pulse models.Pulse) ([]models.JetDrop, error)
 	// GetJetDropsWithParams returns jetDrops for provided pulse with limit and offset.
-	GetJetDropsWithParams(pulse models.Pulse, fromJetDropID *string, limit int, offset int) ([]models.JetDrop, int, error)
-
+	GetJetDropsWithParams(pulse models.Pulse, fromJetDropID *models.JetDropID, limit int, offset int) ([]models.JetDrop, int, error)
+	// GetJetDropByID returns JetDrop by JetDropID
+	GetJetDropByID(id models.JetDropID) (models.JetDrop, error)
 	// GetLifeline returns records for provided object reference, ordered by desc by pulse number and order fields.
 	GetLifeline(objRef []byte, fromIndex *string, pulseNumberLt, pulseNumberGt *int, limit, offset int, sort string) ([]models.Record, int, error)
 }
