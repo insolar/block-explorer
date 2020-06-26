5aee512f4ba03e5f95aef7d3b549bf7d1a205ca7
Tsovak <tsovak@bk.ru>
2020-06-26 12:15:56 +0300

    PENV-259: return migration initial tables

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -17,6 +17,7 @@ func Migrations() []*gormigrate.Migration {
 		{
 			ID: "202005180423",
 			Migrate: func(tx *gorm.DB) error {
+				// the initial database tables. Do not delete it's
 				type Pulse struct {
 					PulseNumber     int `gorm:"primary_key;auto_increment:false"`
 					PrevPulseNumber int
@@ -24,6 +25,31 @@ func Migrations() []*gormigrate.Migration {
 					IsComplete      bool
 					Timestamp       int64
 				}
+				type JetDrop struct {
+					JetID          string `gorm:"type:varchar(255);primary_key;auto_increment:false"`
+					PulseNumber    int    `gorm:"primary_key;auto_increment:false"`
+					FirstPrevHash  []byte
+					SecondPrevHash []byte
+					Hash           []byte
+					RawData        []byte
+					Timestamp      int64
+					RecordAmount   int
+				}
+				type Record struct {
+					Reference           models.Reference `gorm:"primary_key;auto_increment:false"`
+					Type                models.RecordType
+					ObjectReference     models.Reference
+					PrototypeReference  models.Reference
+					Payload             []byte
+					PrevRecordReference models.Reference
+					Hash                []byte
+					RawData             []byte
+					JetID               string
+					PulseNumber         int
+					Order               int
+					Timestamp           int64
+				}
+
 				if err := tx.CreateTable(&Pulse{}).Error; err != nil {
 					return err
 				}
@@ -31,28 +57,28 @@ func Migrations() []*gormigrate.Migration {
 					return err
 				}
 
-				if err := tx.CreateTable(&models.JetDrop{}).Error; err != nil {
+				if err := tx.CreateTable(&JetDrop{}).Error; err != nil {
 					return err
 				}
-				if err := tx.Model(models.JetDrop{}).AddIndex("idx_pulsenumber_jetid", "pulse_number", "jet_id").Error; err != nil {
+				if err := tx.Model(JetDrop{}).AddIndex("idx_pulsenumber_jetid", "pulse_number", "jet_id").Error; err != nil {
 					return err
 				}
-				if err := tx.Model(&models.JetDrop{}).AddForeignKey("pulse_number", "pulses(pulse_number)", "CASCADE", "CASCADE").Error; err != nil {
+				if err := tx.Model(&JetDrop{}).AddForeignKey("pulse_number", "pulses(pulse_number)", "CASCADE", "CASCADE").Error; err != nil {
 					return err
 				}
 
-				if err := tx.CreateTable(&models.Record{}).Error; err != nil {
+				if err := tx.CreateTable(&Record{}).Error; err != nil {
 					return err
 				}
-				if err := tx.Model(models.Record{}).AddIndex(
+				if err := tx.Model(Record{}).AddIndex(
 					"idx_objectreference_type_pulsenumber_order", "object_reference", "type", "pulse_number", "order").Error; err != nil {
 					return err
 				}
-				if err := tx.Model(models.Record{}).AddIndex(
+				if err := tx.Model(Record{}).AddIndex(
 					"idx_jetid_pulsenumber_order", "jet_id", "pulse_number", "order").Error; err != nil {
 					return err
 				}
-				if err := tx.Model(&models.Record{}).AddForeignKey("jet_id, pulse_number", "jet_drops(jet_id, pulse_number)", "CASCADE", "CASCADE").Error; err != nil {
+				if err := tx.Model(&Record{}).AddForeignKey("jet_id, pulse_number", "jet_drops(jet_id, pulse_number)", "CASCADE", "CASCADE").Error; err != nil {
 					return err
 				}
 				return nil
