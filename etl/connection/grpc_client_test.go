0c6439b774d883c324c2e34cf63c7b590b4a1151
Tsovak <tsovak@bk.ru>
2020-07-10 12:51:51 +0300

    PENV-453: add GRPC client tests

--- etl/connection/grpc_client_test.go
+++ etl/connection/grpc_client_test.go
@@ -8,16 +8,16 @@
 package connection
 
 import (
-	"context"
 	"testing"
 
 	"github.com/insolar/block-explorer/configuration"
+	"github.com/insolar/block-explorer/instrumentation/belogger"
 	"github.com/stretchr/testify/require"
 )
 
 func TestNewClient_readyToConnect(t *testing.T) {
 	config := testConfig()
-	client, err := NewGRPCClientConnection(context.Background(), config)
+	client, err := NewGRPCClientConnection(belogger.TestContext(t), config)
 	require.NoError(t, err)
 	defer client.GetGRPCConn().Close()
 	require.NotNil(t, client.GetGRPCConn())
@@ -28,4 +28,33 @@ func testConfig() configuration.Replicator {
 		Addr:            "127.0.0.1:0",
 		MaxTransportMsg: 1073741824,
 	}
+}
+
+func TestConnection_GRPC_auth_with_tls(t *testing.T) {
+	cfg := testConfig()
+	cfg.Auth.Required = true
+	cfg.Auth.InsecureTLS = false
+	client, err := NewGRPCClientConnection(belogger.TestContext(t), cfg)
+	require.NoError(t, err)
+	defer client.GetGRPCConn().Close()
+	require.NotNil(t, client.GetGRPCConn())
+}
+
+func TestConnection_GRPC_auth_insecure(t *testing.T) {
+	cfg := testConfig()
+	cfg.Auth.Required = true
+	cfg.Auth.InsecureTLS = true
+	client, err := NewGRPCClientConnection(belogger.TestContext(t), cfg)
+	require.NoError(t, err)
+	defer client.GetGRPCConn().Close()
+	require.NotNil(t, client.GetGRPCConn())
+}
+
+func TestConnection_GRPC_no_auth(t *testing.T) {
+	cfg := testConfig()
+	cfg.Auth.Required = false
+	client, err := NewGRPCClientConnection(belogger.TestContext(t), cfg)
+	require.NoError(t, err)
+	defer client.GetGRPCConn().Close()
+	require.NotNil(t, client.GetGRPCConn())
 }
