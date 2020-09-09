88fc8bc100803b4f8f60f9e1cd078252897a5063
Mikhail Popov <mikhail.popov@insolar.io>
2020-09-09 14:59:00 +0300

    MN-717: fix unit test

--- etl/extractor/platform_test.go
+++ etl/extractor/platform_test.go
@@ -96,7 +96,7 @@ func TestGetJetDrops_WrongVersionOnPulseError(t *testing.T) {
 	}
 	pulseExtractor := mock.NewPulseExtractorMock(mc)
 	pulseExtractor.GetNextFinalizedPulseMock.Set(func(ctx context.Context, p int64) (fp1 *exporter.FullPulse, err error) {
-		return nil, errors.New("unknown heavy_version")
+		return nil, errors.New("unknown heavy-version")
 	})
 	extractor := NewPlatformExtractor(uint32(1), 0, 100, pulseExtractor, recordClient, shutdownBETestFunc)
 	err := extractor.Start(ctx)
